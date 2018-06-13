1c5507be466f2f570b56eabd1b1ea556569d5eea
Markus Hinsche <m.hinsche@gmail.com>
2018-06-13 12:39:28 +0200

    Use https, because this is already default on uberspace v7

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,2 +1,2 @@
 export const BASE_URL =
-  process.env.NODE_ENV === "production" ? "http://ambience.uber.space/api" : "http://localhost:62729";
+  process.env.NODE_ENV === "production" ? "https://ambience.uber.space/api" : "http://localhost:62729";

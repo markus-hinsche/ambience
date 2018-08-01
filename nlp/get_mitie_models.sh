e4288ccaf4533acb7e65fb09f973784e8c3e8d25
Markus Hinsche <m.hinsche@gmail.com>
2018-08-01 22:04:05 +0200

    Untar

--- nlp/get_mitie_models.sh
+++ nlp/get_mitie_models.sh
@@ -6,3 +6,4 @@ cd nlp
 wget https://github.com/mit-nlp/MITIE/releases/download/v0.4/MITIE-models-v0.2.tar.bz2
 tar jxf MITIE-models-v0.2.tar.bz2
 bzip2 -dk MITIE-models-v0.2.tar.bz2
+tar xvf MITIE-models-v0.2.tar
